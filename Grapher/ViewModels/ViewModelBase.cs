869e502ff505707626dca3a5f77b21b7587d39b5
1ikuszak <luke.glica@gmail.com>
2023-09-21 23:11:57 +0200

    First commit

---
+++ Grapher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Grapher.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Grapher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Grapher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
